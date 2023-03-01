d8c750964e33fc491cbfb7d7158d26273fa47e08
LEE-YO-HAN <yhl0078@gmail.com>
2023-03-01 23:59:24 +0900

    Update: setup basic SEO

--- src/components/post/NextHead.tsx
+++ src/components/post/NextHead.tsx
@@ -12,6 +12,7 @@ export const NextHead = ({ title, description, category }: Props) => {
       <title>Rain Suagr BangBang Front-End</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={`${description}`} />
+      <meta name="keywords" content={`${category}, ${category}-${title}`} />
       <meta property="og:title" content={`${title}`} />
       <meta property="og:description" content={`${description}`} />
       <meta
